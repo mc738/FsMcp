49c0a75c9898cf16651704965471b0222d5ad1de
Max <me@mclifford.dev>
2025-06-15 21:43:40 +0100

    chore(projectrisk): Added project

---
+++ FsMcp.Server.ProjectRisk/Program.fs
@@ -0,0 +1,18 @@
+﻿
+open Freql.Csv
+
+module Test =
+    
+    let run _ =
+        let (rows, errors) =
+            CsvParser.parseFileV2<FsMcp.Server.ProjectRisk.Data.Preparation.Common.RawValue> true "C:\\Users\\mclif\\Downloads\\archive (2)\\project_risk_raw_dataset.csv"
+            |> CsvParser.splitResults
+        
+        
+        ()
+        
+
+Test.run ()
+
+// For more information see https://aka.ms/fsharp-console-apps
+printfn "Hello from F#"
