5c1e0368e6395f22364bd3697111c4eb3318c11c
Max <me@mclifford.dev>
2025-06-15 18:12:28 +0100

    chore(core): Work on server

--- FsMcp.ExampleServer/Program.fs
+++ FsMcp.ExampleServer/Program.fs
@@ -18,6 +18,41 @@ type MyService() =
         member this.Test() = ()
         member this.GetMessage() = "Hello from MyService"
 
+type Product =
+    {
+        Name: string
+        Value: decimal
+        Tags: string list
+    }
+    
+let products =
+    [
+        { Name = "Foo product 1"
+          Value = 100m
+          Tags = [ "foo" ] }
+        
+        { Name = "Foo product 2"
+          Value = 40m
+          Tags = [ "foo" ] }
+        
+        { Name = "Foo product 3"
+          Value = 55m
+          Tags = [ "foo" ] }
+        
+        { Name = "Foo product 4"
+          Value = 59.99m
+          Tags = [ "foo" ] }
+        
+        { Name = "Foo product 5"
+          Value = 50m
+          Tags = [ "foo" ] }
+        
+        
+        { Name = "Bar product 1"
+          Value = 50m
+          Tags = [ "bar" ] }
+    ]
+
 module Tools =
 
     [<AbstractClass; Sealed>]
@@ -31,6 +66,11 @@ module Tools =
         [<McpServerTool; Description("Echoes in reverse the message sent by the client.")>]
         static member ReverseEcho(test: IMyService, message: string) =
             message |> Seq.rev |> Seq.toArray |> String
+            
+            
+        [<McpServerTool; Description("Get a list of products based on a tag")>]
+        static member GetProducts(test: IMyService, [<Description("The tag to search for")>] tag: string) =
+            products |> Seq.filter (fun product -> product.Tags |> List.contains tag)
 
 let build _ =
 
