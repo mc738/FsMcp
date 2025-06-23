735b0b58bfff8c6e237ee73aa731aeb4868f80b1
Max <me@mclifford.dev>
2025-06-23 22:37:07 +0100

    chore(core): Work on data import

--- FsMcp.Server.ProjectRisk/Program.fs
+++ FsMcp.Server.ProjectRisk/Program.fs
@@ -1,5 +1,7 @@
 ﻿
 open Freql.Csv
+open Freql.Sqlite
+open FsMcp.Server.ProjectRisk.Data.Preparation
 
 module Test =
     
@@ -7,6 +9,15 @@ module Test =
         let (rows, errors) =
             CsvParser.parseFileV2<FsMcp.Server.ProjectRisk.Data.Preparation.Common.RawValue> true "C:\\Users\\mclif\\Downloads\\archive (2)\\project_risk_raw_dataset.csv"
             |> CsvParser.splitResults
+        
+        use ctx = SqliteContext.Create("_raw")
+        
+        ctx.CreateTable<FsMcp.Server.ProjectRisk.Data.Preparation.Common.RawValue>("_raw") |> ignore
+        
+        rows
+        |> List.iter (fun value -> ctx.Insert<RawValue>("_raw", value))
+        
+        
         
         
         ()
